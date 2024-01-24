c26f7c2200a53cae8ebbd73ae0e1098d2fbc7327
myuto <m.yuto72319@gmail.com>
2024-01-25 00:09:16 +0900

    構造体定義

--- model/diary/diary.go
+++ model/diary/diary.go
@@ -4,7 +4,32 @@ import (
 	"fmt"
 	"shisha-log-backend/lib"
 	"shisha-log-backend/model"
+	"shisha-log-backend/model/bottle"
+	"shisha-log-backend/model/bowl"
+	"shisha-log-backend/model/charcoal"
+	"shisha-log-backend/model/fravor"
+	"shisha-log-backend/model/heatmanagement"
 )
+
+type Diary struct {
+	Fravor           fravor.Fravors
+	Bottle           bottle.Bottle
+	Bowl             bowl.Bowl
+	HeatManagement   heatmanagement.HeatManagement
+	Chacoal          charcoal.Charcoal
+	Temperature      *float64
+	Humidity         *float64
+	CreatorGoodPoint *string
+	CreatorBadPoint  *string
+	CreatorStar      int
+	TasteStar        int
+}
+
+type userDiary struct {
+}
+
+type FlavorMix struct {
+}
 
 type Diaries struct {
 	Items []model.Diary
