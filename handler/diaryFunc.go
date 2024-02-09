eb29567af7ef348354cdb1d7fa6d1a48206b907f
myuto <m.yuto72319@gmail.com>
2024-02-09 20:46:38 +0900

    add: diary_equipmentsにデータを追加する処理

--- handler/diaryFunc.go
+++ handler/diaryFunc.go
@@ -1,9 +1,10 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+	"shisha-log-backend/lib"
 	"shisha-log-backend/model/diary"
+	"shisha-log-backend/model/equipment"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -23,6 +24,8 @@ func GetUserDiaries(userDiaries *diary.UserDiaries) gin.HandlerFunc {
 func CreateDiary(c *gin.Context) {
 	var req diary.DiaryRequest
 	var diaries diary.Diaries
+	var diaryEquipments equipment.DiaryEquipments
+
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -31,14 +34,25 @@ func CreateDiary(c *gin.Context) {
 	diaryID, err := uuid.New().MarshalBinary()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 	diaryEquipmentsID, err := uuid.New().MarshalBinary()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
-	// テストのために一時的にdiary_equipments_idの外部キー制約外してるから戻す
-	item := diary.Diary{
+	// テストのために一時的にimage_idの外部キー制約外してるから戻す
+	diaryEquipmentsItem := equipment.DiaryEquipment{
+		ID:                   diaryEquipmentsID,
+		UserBowlID:           lib.ParseUUIDStrToBin(req.Equipments.BowlID),
+		UserBottleID:         lib.ParseUUIDStrToBin(req.Equipments.BottleID),
+		UserHeatManagementID: lib.ParseUUIDStrToBin(req.Equipments.HeatManagementID),
+		UserCharcoalID:       lib.ParseUUIDStrToBin(req.Equipments.CharcoalID),
+		DiaryImageID:         lib.ParseUUIDStrToBin(req.ImageID),
+	}
+
+	diaryItem := diary.Diary{
 		ID:                diaryID,
 		DiaryEquipmentsID: diaryEquipmentsID,
 		SuckingText:       &req.SuckingText,
@@ -52,8 +66,16 @@ func CreateDiary(c *gin.Context) {
 		CreateDate:        stringToTime(req.CreateDate),
 	}
 
-	fmt.Println(item)
-	diaries.Add(item)
+	err = diaryEquipments.Add(diaryEquipmentsItem)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	err = diaries.Add(diaryItem)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Successful operation"})
 }
