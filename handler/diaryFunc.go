98a822184fffdc44884b62de3e38b2f5de84f8a2
myuto <m.yuto72319@gmail.com>
2024-02-16 23:35:30 +0900

    fix: 日付のフォーマット修正

--- handler/diaryFunc.go
+++ handler/diaryFunc.go
@@ -117,7 +117,7 @@ func CreateDiary(c *gin.Context) {
 		CreatorGoodPoints: &req.Review.CreatorGoodPoints,
 		CreatorBadPoints:  &req.Review.CreatorBadPoints,
 		TasteComments:     &req.Review.TasteComments,
-		CreateDate:        stringToTime(req.CreateDate),
+		CreateDate:        lib.StringToTime(req.CreateDate),
 	}
 
 	diaryFlavorItems := []flavor.PostDiaryFlavor{}
