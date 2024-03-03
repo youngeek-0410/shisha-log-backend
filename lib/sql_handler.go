62c0c7ae8d89e0e64315a4615333038bcc8ae6d6
myuto <m.yuto72319@gmail.com>
2024-03-03 10:46:59 +0900

    Chore

--- lib/sql_handler.go
+++ lib/sql_handler.go
@@ -50,10 +50,9 @@ func DBClose() {
 // NewSQLHandler ...
 func NewSQLHandler() *SQLHandler {
 	var db *gorm.DB
-	var dsn string
 	var err error
 
-	dsn = GenerateDsn()
+	dsn := GenerateDsn()
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
